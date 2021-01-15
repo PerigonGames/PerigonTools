c5ef8605381b53c91078cb2ce626906983299a5f
Gursaanj <gursaanjresides@gmail.com>
2021-01-15 00:12:35 -0800

    Vector3Extensions

---
+++ Assets/Scripts/Vector3Extensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PerigonGames
+{
+    public static class Vector3Extensions
+    {
+        /*
+        Explanation of what it does: resets vector
+        Category: Vector3
+        Sample Input: object.Transform.position
+        Expected Output: vector reset to Vector3.zero
+    */
+        public static void Reset(this ref Vector3 v)
+        {
+            v = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Tests/Vector3ExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PerigonGames;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Vector3ExtensionsTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ResetToVector3Zero()
+        {
+            var v = new Vector3(0,2,1);
+            v.Reset();
+            
+            Assert.AreEqual(Vector3.zero, v);
+        }
+    }
+}
